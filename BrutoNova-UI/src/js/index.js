b26bfb2d6af26cb13f0856c6727ef6018514f498
4xx404 <45526353+4xx404@users.noreply.github.com>
2021-06-24 20:38:28 +0000

    Create index.js

---
+++ BrutoNova-UI/src/js/index.js
@@ -0,0 +1,15 @@
+function md5Redirect(){
+	window.location.replace('md5-hashes.php');
+}
+
+function ftpRedirect(){
+	window.location.replace('ftp-passwords.php');
+}
+
+function sshRedirect(){
+	window.location.replace('ssh-passwords.php');
+}
+
+function webRedirect(){
+	window.location.replace('web-logins.php');
+}
